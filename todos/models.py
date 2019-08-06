b96de6df66cdc63a3b64ebf39aab1c669426e671
Ubuntu <ubuntu@ip-172-31-22-168.ap-southeast-1.compute.internal>
2019-08-06 08:43:19 +0000

    Added a basic model

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+# declare a class named "Item"
+class Item(models.Model):
+    name = models.CharField(max_length=30, blank=False)
+    done = models.BooleanField(blank=False, default=False)
